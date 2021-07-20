6545a163a2df804a37de529c7fc2cb32bf6a5d25
Nightstars <christzhangowner#gmail.com>
2021-07-21 01:01:44 +0800

    add nuget support

--- WpfUpdate/AssemblyInfo.cs
+++ WpfUpdate/AssemblyInfo.cs
@@ -1,10 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                                     //(used if a resource is not found in the page,
-                                     // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                              //(used if a resource is not found in the page,
-                                              // app, or any theme specific resource dictionaries)
-)]
+[assembly: AssemblyTitle("SmartSoft Studio")]
+[assembly: AssemblyDescription("SmartUpdate .NET Standard")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SmartSoft Studio")]
+[assembly: AssemblyProduct("SmartUpdate .Net")]
+[assembly: AssemblyCopyright("Copyright © SmartSoft Studio 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components. If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("33BEA85E-D6F0-65D1-CC21-3727DE33FAB0")]
+
+// Version information for an assembly consists of the following four values:
+//
+// Major Version
+// Minor Version
+// Build Number
+// Revision
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
