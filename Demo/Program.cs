3470951b02ee282ca30eb7b4920ed1a3ff1c9f1e
Nightstars <ChristZhangowner@gmail..com>
2021-07-21 17:34:28 +0800

    add new features

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,21 +1,27 @@
-﻿using System;
+﻿using SmartSoft.common.Utils.solution;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Demo
 {
     class Program
     {
-        static async Task Main(string[] args)
+        //static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            using var httpClient = new HttpClient();
-            var url = "http://192.168.2.114:10002/upload/smarttools/2021/7/2021715003/Update_20210715491258281.zip";
-            byte[] imageBytes = await httpClient.GetByteArrayAsync(url).ConfigureAwait(false);
-            //var responseMessage = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false)
-            string localFilename = "update.zip";
-            string localPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, localFilename);
-            File.WriteAllBytes(localPath, imageBytes);
+            //using var httpClient = new HttpClient();
+            //var url = "http://192.168.2.114:10002/upload/smarttools/2021/7/2021715003/Update_20210715491258281.zip";
+            //byte[] imageBytes = await httpClient.GetByteArrayAsync(url).ConfigureAwait(false);
+            ////var responseMessage = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false)
+            //string localFilename = "update.zip";
+            //string localPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, localFilename);
+            //File.WriteAllBytes(localPath, imageBytes);
+
+            List<SolutionInfo> list= new SolutionUtil(@"D:\myworkspace\V6\v6-xinjin\Longnows.Saas.XinJing.sln").SlnParse();
             Console.Read();
         }
     }
--- WpfMainApp/App.xaml.cs
+++ WpfMainApp/App.xaml.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Threading.Tasks;
+﻿using SmartSoft.common.Utils;
+using SmartSoft.common.Utils.solution;
+using System;
 using System.Windows;
-using WpfMainApp.Util;
+
 
 namespace WpfMainApp
 {
@@ -20,15 +14,6 @@ namespace WpfMainApp
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             new UpdateUtil().checckUpdate("./WpfMainApp.exe", "http://192.168.2.114:10002");
-
-            string currentVersion = "1.0.0";
-            string lastVersion="1.0.1";
-            string logHtml = "http://cloudapps.life:9003/";
-            string updateUrl = "http://192.168.0.103:8080/Update.zip";
-            string installPath = AppDomain.CurrentDomain.BaseDirectory;
-            string updateZipMD5 = "1111";
-            //Process.Start("Update.exe", $"{currentVersion}|{lastVersion}|{logHtml}|{updateUrl}|{installPath}|{updateZipMD5}");
-            //Process.GetCurrentProcess().Kill();
         }
     }
 }
